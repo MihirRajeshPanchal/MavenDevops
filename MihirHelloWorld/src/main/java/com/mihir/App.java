7b8f53eeb718623a1ad7654a335304261beb6088
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2025-02-15 11:46:11 +0530

    Add initial project structure and basic functionality

---
+++ MihirHelloWorld/src/main/java/com/mihir/App.java
@@ -0,0 +1,14 @@
+package com.mihir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+        System.out.println("My name is Mihir");
+    }
+}
